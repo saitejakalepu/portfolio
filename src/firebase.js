5236a4f1ce6db478da7949777642d1e8a28a9385
saitejakalepu <saiteja.kalepu@gmail.com>
2022-08-26 22:14:05 +0530

    removed config details

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYM-fU-B_B5PBHaUgNY8mSzflm1XZOLWY",
-  authDomain: "saitejakalepu-portfolio.firebaseapp.com",
-  databaseURL: "https://saitejakalepu-portfolio-default-rtdb.firebaseio.com",
-  projectId: "saitejakalepu-portfolio",
-  storageBucket: "saitejakalepu-portfolio.appspot.com",
-  messagingSenderId: "701383325828",
-  appId: "1:701383325828:web:77ea0311781f0a95736ad1",
-  measurementId: "G-XV2NREL0X6"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
