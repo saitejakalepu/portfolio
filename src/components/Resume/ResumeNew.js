87a69835d2bd44c720a89b0344260c8a669a0b1a
saitejakalepu <saiteja.kalepu@gmail.com>
2024-01-20 09:37:44 +0530

    resume file format on save

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,25 +1,25 @@
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import { useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
-  const url = useSelector(state => state.data.resume);
+  const url = useSelector((state) => state.data.resume);
 
-  useEffect(()=>{
+  useEffect(() => {
     setWidth(window.innerWidth);
-   },[])
+  }, []);
 
   return (
     <div>
       <Container fluid className="resume-section">
-      <Particle />
+        <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
@@ -37,7 +37,7 @@ function ResumeNew() {
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
-        
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
